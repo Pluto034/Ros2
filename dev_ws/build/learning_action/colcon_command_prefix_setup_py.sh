c143a24fbb31676bd7fdd59c113f00d71c6658b8
“pluto12” <1662115200@qq.com>
2024-10-10 10:11:05 +0800

    Init

---
+++ dev_ws/build/learning_action/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/RosDemo/dev_ws/install/learning_interface/share/learning_interface/package.sh"
---
+++ dev_ws/build/learning_topic/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/RosDemo/dev_ws/install/learning_topic'
